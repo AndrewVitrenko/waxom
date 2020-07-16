711b6209f8feae02ecde29436bc1c56bfc1525a2
Andrew Vitrenko <57104862+AndrewVitrenko@users.noreply.github.com>
2020-07-16 20:26:34 +0300

    new

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(window).ready(function() {
+    $('.slider').slick({
+        dots: true,
+        slidesToShow: 1,
+        infinite: false,
+    })
+
+    $('.banner').css('height', $('.banner__background').outerHeight())
+    $('.presentation').css('height', $('.presentation__background').outerHeight())
+    $('.partners').css('height', $('.partners__background').outerHeight())
+})
+
+$(window).resize(function() {
+    $('.banner').css('height', $('.banner__background').outerHeight())
+    $('.presentation').css('height', $('.presentation__background').outerHeight())
+    $('.partners').css('height', $('.partners__background').outerHeight())
+})
